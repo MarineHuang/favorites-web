560adbfd541bf6b35a0c166d4a81b9991e44f55e
MarineHuang <huang_yx005@163.com>
2019-11-11 14:09:15 +0800

    download asr result

--- app/src/main/java/com/favorites/web/AudioController.java
+++ app/src/main/java/com/favorites/web/AudioController.java
@@ -1,12 +1,18 @@
 package com.favorites.web;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -81,6 +87,56 @@ public class AudioController extends BaseController implements ApplicationRunner
 			logger.error("导入html异常:",e);
 		}
 		return "audio/audiolist";
+	}
+	
+    /**
+	 * 下载转写结果
+	 *
+	 */
+	@RequestMapping(value="/download", method = RequestMethod.GET)
+	@LoggerManage(description="下载转写结果操作")
+	public String downLoad(@RequestParam("audioId") Long audioId, HttpServletResponse response){
+		Audio audio = audioRepository.findById(audioId).get();
+		String path = audio.getResultFullPath();
+		File file = new File(path);
+		String filename = file.getName();
+		//判断文件父目录是否存在
+		if(file.exists()){
+			response.setHeader("content-type", "application/octet-stream");
+			response.setContentType("application/octet-stream");
+			response.setHeader("Content-Disposition", "attachment;filename=" + filename);
+			
+			byte[] buffer = new byte[1024];
+			FileInputStream fis = null;
+			BufferedInputStream bis = null;
+			OutputStream os = null;
+			try {
+			 // 获取响应对象的输出流
+			 os = response.getOutputStream();
+			 // 文件输入流
+			 fis = new FileInputStream(file);
+			 // 缓存输入流
+			 bis = new BufferedInputStream(fis);
+			 // 数组从输入流缓存读取数据,返回数据是读取的byte个数
+			 int i = bis.read(buffer);
+			 while(i != -1){
+			     os.write(buffer, 0, i);
+			     i = bis.read(buffer);
+			 	}
+			} catch (Exception e) {
+			 e.printStackTrace();
+			}
+			try {
+				 assert bis != null;
+				 bis.close();
+				 fis.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}else {
+			logger.info("file not exits:" + path);
+		}
+		return null;
 	}
 
 }
